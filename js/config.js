5bbaa246afd1cfd463341b064126878a701eabce
AgustinRoss39 <104650789+AgustinRoss39@users.noreply.github.com>
2026-09-18 20:06:26 -0300

    Desactiva destino de confirmación en demo

--- js/config.js
+++ js/config.js
@@ -11,7 +11,7 @@ window.INVITATION_CONFIG = {
     address: "Cancha de fútbol “Fútbol 10”",
     mapUrl: "https://www.google.com/maps/place/F%C3%BAtbol+10/@-34.5894853,-58.6524657,17z/data=!3m1!4b1!4m6!3m5!1s0x95bcbeb4c1782ee1:0xf301e4afe3f20361!8m2!3d-34.5894854!4d-58.6478523!16s%2Fg%2F11cs2h3xlw?entry=ttu",
     dressCode: "¡Venite con la camiseta de la Selección Argentina y listo para jugar!",
-    whatsappNumber: "5491156223007",
+    whatsappNumber: "",
     whatsappMessage: "¡Hola! Confirmo mi asistencia al cumpleaños de Juan. 🇦🇷⚽",
     instagramUrl: "https://www.instagram.com/rossdigitalstudio/",
     instagramHandle: "@rossdigitalstudio"
